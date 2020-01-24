9e9bdd7a3423488e9874fff3abbcc77561f4b6d2
marcin.brzeczek <Morris.150>
2020-01-24 13:30:01 +0100

    Use lifecycle observer.

--- app/src/main/java/upday/droidconmvvm/MainActivity.kt
+++ app/src/main/java/upday/droidconmvvm/MainActivity.kt
@@ -6,10 +6,8 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.Spinner
 import android.widget.TextView
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.functions.Consumer
+import androidx.lifecycle.Observer
+
 import upday.droidconmvvm.model.Language
 
 class MainActivity : AppCompatActivity() {
@@ -20,61 +18,52 @@ class MainActivity : AppCompatActivity() {
 
     private var mLanguageSpinnerAdapter: LanguageSpinnerAdapter? = null
 
-    private val viewModel: MainViewModel by lazy {(application as DroidconApplication).viewModel}
+    private val viewModel: MainViewModel by lazy { (application as DroidconApplication).viewModel }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupViews()
+        getSupportedLanguages()
+        observe()
+    }
+
+    private fun observe() {
+        viewModel.greetingsList.observe(this, Observer { languages ->
+            languages?.let {
+                setLanguages(it)
+            }
+        })
+        viewModel.greetingText.observe(this, Observer { text ->
+            text?.let {
+                setGreeting(it)
+            }
+        })
     }
 
     private fun setupViews() {
         mGreetingView = findViewById(R.id.greeting) as TextView?
-
         mLanguagesSpinner = findViewById(R.id.languages) as Spinner?
         assert(mLanguagesSpinner != null)
         mLanguagesSpinner!!.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>, view: View,
-                                        position: Int, id: Long) {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
                 itemSelected(position)
             }
-
-            override fun onNothingSelected(parent: AdapterView<*>) {
-                //nothing to do here
-            }
+            override fun onNothingSelected(parent: AdapterView<*>) {}
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        bind()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        unBind()
-    }
-
-    private fun bind() {
-
-        mCompositeDisposable.add(mViewModel.supportedLanguages
-            .subscribeOn(Schedulers.computation())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(Consumer<List<Language>> { this.setLanguages(it) }))
-    }
+    private fun getSupportedLanguages() = viewModel.supportedLanguages
 
     private fun setGreeting(greeting: String) {
         assert(mGreetingView != null)
-
         mGreetingView!!.text = greeting
     }
 
     private fun setLanguages(languages: List<Language>) {
         assert(mLanguagesSpinner != null)
 
-        mLanguageSpinnerAdapter = LanguageSpinnerAdapter(this,
-            R.layout.language_item,
-            languages)
+        mLanguageSpinnerAdapter = LanguageSpinnerAdapter(this, R.layout.language_item, languages)
         mLanguagesSpinner!!.adapter = mLanguageSpinnerAdapter
     }
 
@@ -82,6 +71,6 @@ class MainActivity : AppCompatActivity() {
         assert(mLanguageSpinnerAdapter != null)
 
         val languageSelected = mLanguageSpinnerAdapter!!.getItem(position)
-        mViewModel.languageSelected(languageSelected!!)
+        viewModel.languageSelected(languageSelected!!)
     }
 }
--- app/src/main/java/upday/droidconmvvm/MainViewModel.kt
+++ app/src/main/java/upday/droidconmvvm/MainViewModel.kt
@@ -1,26 +1,18 @@
 package upday.droidconmvvm
 
-import android.location.Location
 import androidx.lifecycle.MutableLiveData
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
 import io.reactivex.subjects.BehaviorSubject
 import upday.droidconmvvm.datamodel.IDataModel
 import upday.droidconmvvm.model.Language
-import upday.droidconmvvm.model.LanguageCode
 import upday.droidconmvvm.schedulers.ISchedulerProvider
 
-/**
- * View model for the main activity.
- */
 class MainViewModel(
     mDataModel: IDataModel,
     mSchedulerProvider: ISchedulerProvider
 ) : BaseViewModel() {
 
     val greetingText = MutableLiveData<String>()
-    val greetingsList = MutableLiveData<List<String>>()
+    val greetingsList = MutableLiveData<List<Language>>()
 
     private val mSelectedLanguage = BehaviorSubject.create<Language>()
 
@@ -32,13 +24,12 @@ class MainViewModel(
         .subscribe { greetingText.value = it }
         .collect()
 
-
     val supportedLanguages = mDataModel.supportedLanguages
         .subscribeOn(mSchedulerProvider.computation())
         .observeOn(mSchedulerProvider.ui())
         .subscribe { greetingsList.value = it }
         .collect()
-e
+
     fun languageSelected(language: Language) {
         mSelectedLanguage.onNext(language)
     }
